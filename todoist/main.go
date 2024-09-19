4d63b4f48ac4aa17cdc1b3722188ea6fce3ff843
rupokghosh <rupokghosh291@gmail.com>
2024-09-19 14:34:32 -0700

    add main package code

--- todoist/main.go
+++ todoist/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main ()
+import (
+    "fmt"
+    "log"
+    "os"
+    "todoist-cli/cmd"
+)
+
+func main() {
+    if err := cmd.RootCmd.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+}
