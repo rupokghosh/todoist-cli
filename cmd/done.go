7cdf763ab78b4c4c60f75b301a7bf3736ab5249d
rupokghosh <rupokghosh291@gmail.com>
2024-10-14 12:49:46 -0700

    setup done command

--- cmd/done.go
+++ cmd/done.go
@@ -1,6 +1,18 @@
 package cmd
 
-func Done() {
+import (
+	"github.com/spf13/cobra"
+	"fmt"
+	
+)
 
-
+func init() {
+	rootCmd.AddCommand(doneCmd)
 }
+var doneCmd = &cobra.Command{
+	Use: "done", 
+	Short: "Command to mark a task as done", 
+	Run: func (cmd *cobra.Command, args []string){
+		fmt.Println("done!")
+	},
+ }
